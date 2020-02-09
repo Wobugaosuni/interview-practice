6c9af1b54f6481e96fa6958b33e3992f6d1b476b
qiuming <397401374@qq.com>
2020-02-09 18:18:46 +0800

    error

--- app/containers/react-error/error.js
+++ app/containers/react-error/error.js
@@ -3,28 +3,32 @@ import React from 'react'
 export default class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { error: null, errorInfo: null }
-  }
-  
-  componentDidCatch(error, errorInfo) {
-    // Catch errors in any components below and re-render with error message
-    this.setState({
-      error: error,
-      errorInfo: errorInfo
-    })
-    // You can also log error messages to an error reporting service here
+    this.state = { hasError: null, errorInfo: null }
   }
 
+  static getDerivedStateFromError(error) {
+    return {hasError: true}
+  }
+  
+  // componentDidCatch(error, errorInfo) {
+  //   // Catch errors in any components below and re-render with error message
+  //   this.setState({
+  //     hasError: error,
+  //     errorInfo: errorInfo
+  //   })
+  //   // You can also log error messages to an error reporting service here
+  // }
+
   render() {
-    if (this.state.errorInfo) {
+    if (this.state.hasError) {
       // Error path
       return (
         <div>
           <h2>Something went wrong.</h2>
           <details style={{ whiteSpace: 'pre-wrap' }}>
-            {this.state.error && this.state.error.toString()}
+            {this.state.hasError && this.state.hasError.toString()}
             <br />
-            {this.state.errorInfo.componentStack}
+            {/* {this.state.errorInfo.componentStack} */}
           </details>
         </div>
       )
--- app/containers/react-error/index.md
+++ app/containers/react-error/index.md
@@ -2,11 +2,14 @@
 
 当抛出错误后，请使用 `static getDerivedStateFromError()` 渲染备用 UI ，使用 `componentDidCatch()` 打印错误信息
 
+>注意
+如果发生错误，你可以通过调用 `setState` 使用 `componentDidCatch()` 渲染降级 UI，但在未来的版本中将不推荐这样做。 可以使用静态 `getDerivedStateFromError()` 来处理降级渲染。
+
 ## 错误边界
 react的组件  
 可以捕获并打印发生在其**子组件树**任何位置的 JavaScript 错误，并且，它会渲染出备用 UI
 
-## 错误边界无法捕获以下场景中产生的错误：
+## 错误边界**无法捕获**以下场景中产生的错误：
 1. 事件处理
 2. 异步代码（例如 setTimeout 或 requestAnimationFrame 回调函数）
 3. 服务端渲染
